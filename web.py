19d5b398b407b1588559e1bfb3b67ffb824bfe7c
openspace-vr <boone.sheena@gmail.com>
2025-07-29 23:55:00 -0400

    added an add activity feature

--- web.py
+++ web.py
@@ -1,9 +1,16 @@
-from logging import PlaceHolder
-
 import streamlit as st
 from modules import functions
 
 todos = functions.get_todos()
+
+def add_todo():
+    new_todo = st.session_state["activity"]
+    todos.append(new_todo)
+    #functions.cursor_position()
+    functions.write_todos(todos)
+    #print(todo)
+
+
 
 
 st.title('My Web App')
@@ -13,5 +20,6 @@ st.write('Create a checklist of activities')
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a new To Do", placeholder="Add activity", key="activity")
+st.text_input(label="Enter a new To Do", placeholder="Add activity",
+              on_change=add_todo, key="activity")
 
