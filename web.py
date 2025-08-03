646b4488b89031458855eee016cb98f42d8332e3
openspace-vr <boone.sheena@gmail.com>
2025-08-03 18:27:08 -0400

    checks and verifies that the next activity starts on a new line.

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit import checkbox
+
 from modules import functions
 
 todos = functions.get_todos()
@@ -6,20 +8,26 @@ todos = functions.get_todos()
 def add_todo():
     new_todo = st.session_state["activity"]
     todos.append(new_todo)
-    #functions.cursor_position()
+    functions.cursor_position()
     functions.write_todos(todos)
     #print(todo)
-
-
-
 
 st.title('My Web App')
 st.subheader('Todos List')
 st.write('Create a checklist of activities')
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key= todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a new To Do", placeholder="Add activity",
               on_change=add_todo, key="activity")
+
+#print("testing")
+#session state type which is a string similar to a dictionary
+#st.session_state
 
