a2b341d190c0d92ea18149e53fb99d1700d859a1
openspace-vr <boone.sheena@gmail.com>
2025-08-03 18:28:05 -0400

    checks and verifies that the next activity starts on a new line.

---
+++ modules/functions.py
@@ -0,0 +1,44 @@
+dynamic_path = "todos.txt"
+""" 
+Functions.py will open the file read the content (get_todos) and
+write the data accordingly (write_todos)
+"""
+print ("Dynamic path defined in functions.py")
+
+""" base_dir = os.path.join("D:","Udemy", "D6")  
+sub_dir = "Bonus"
+dynamic_path = os.path.join(base_dir, sub_dir,"todos.txt") """
+
+def cursor_position():
+    with open(dynamic_path, "a+") as file:
+        while True:
+            line = file.readline()
+            line_length = len(line)
+
+            if line_length > 0 and line[line_length-1] == "\n":
+                #print("new line")
+                pass
+            else:
+                #with open(dynamic_path, "a") as append:
+                #You have to use a+ to append to end of the file
+                file.write("\n")
+            if not line:
+                break
+
+
+def get_todos():
+    with open(dynamic_path, 'r') as file:
+        todos_local = file.readlines() #should be a different name that is local
+    return todos_local
+
+def write_todos(new_todos):
+    with open(dynamic_path, 'w') as file:
+        file.writelines(new_todos)  #This will automatically close the file
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
+
+    #This will only happen in the functions module
+    #__name__ is the name of the function you are currently running (directly). 
+    #inside functions is main outside it is the name of the program
